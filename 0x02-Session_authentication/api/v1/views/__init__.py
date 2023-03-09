6ddc0e8a635efbff44c5038bf66b9d67ccec41d2
ngugimuchangi <tech.curious14@gmail.com>
2023-03-09 05:35:55 +0300

    requirement fix

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -7,9 +7,9 @@ from models.user_session import UserSession
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import *
-from api.v1.views.users import *
-from api.v1.views.session_auth import *
+# from api.v1.views.index import *
+# from api.v1.views.users import *
+# from api.v1.views.session_auth import *
 
-User.load_from_file()
+# User.load_from_file()
 UserSession.load_from_file()
