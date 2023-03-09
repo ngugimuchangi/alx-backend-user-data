328ca29b1849f7716359b4ed25c2b64e375f2776
ngugimuchangi <tech.curious14@gmail.com>
2023-03-09 05:35:02 +0300

    requirement fix

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-""" DocDocDocDocDocDocldldl
+""" DocDocDocDocDocDoc
 """
 from flask import Blueprint
+from models.user_session import UserSession
+
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.session_auth import *
-from models.user_session import UserSession
 
 User.load_from_file()
 UserSession.load_from_file()
