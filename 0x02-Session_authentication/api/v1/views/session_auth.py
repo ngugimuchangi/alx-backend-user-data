858441808b6ddc59a21a3cb91aeab3b66899099d
ngugimuchangi <tech.curious14@gmail.com>
2023-03-09 04:26:59 +0300

    requirement fix

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -11,7 +11,7 @@ from os import getenv
 
 @app_views.route('/auth_session/login', methods=['POST'],
                  strict_slashes=False)
-def login() -> Union[Tuple[Response, int], Response]:
+def login() -> str:
     """ Session authentication route
         POST parameters:
             - email: user's email
@@ -43,7 +43,7 @@ def login() -> Union[Tuple[Response, int], Response]:
 
 @app_views.route('/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
-def logout() -> Response:
+def logout() -> str:
     """ Logs out user and destroys session
         User needs to be logged in with a session
         id associated with their user_id
