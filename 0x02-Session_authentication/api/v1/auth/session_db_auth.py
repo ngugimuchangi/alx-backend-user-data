9f6f31fd6ce094a1eb22b6f2f9d6f8d1548569f5
ngugimuchangi <tech.curious14@gmail.com>
2023-03-09 18:31:53 +0300

    file cleanup

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -5,9 +5,6 @@ Session storage module
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from datetime import datetime, timedelta
 from models.user_session import UserSession
-from models.user import User
-from typing import TypeVar
-from os import getenv
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -51,22 +48,6 @@ class SessionDBAuth(SessionExpAuth):
         SessionDBAuth.user_id_by_session_id.pop(session_id)
         session.remove()
         return None
-
-    # def current_user(self, request=None) -> TypeVar('User'):
-    #     """ Get current user
-    #         Return:
-    #             - User object for user instance whose user id is
-    #               linked to given session id
-    #     """
-    #     if not request:
-    #         return None
-    #     session_id = self.session_cookie(request)
-    #     user_id = self.user_id_for_session_id(session_id)
-
-    #     try:
-    #         return User.get(user_id)
-    #     except KeyError:
-    #         return None
 
     def destroy_session(self, request=None) -> bool:
         """ Destroy session object based on session id
