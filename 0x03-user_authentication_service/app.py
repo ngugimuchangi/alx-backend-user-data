c3a2bcc2ea97016a229b5893001eeab63cfbd3bc
ngugimuchangi <tech.curious14@gmail.com>
2023-03-15 23:03:04 +0300

    user signup route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 """
 Flask app
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+auth = Auth()
 
 
 @app.route("/")
@@ -13,6 +15,19 @@ def home() -> str:
     """ Home endpoint
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user() -> tuple[str, int]:
+    """ Register's new user
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 201
+    except ValueError:
+        return jsonify({"message": "email already register"}), 400
 
 
 if __name__ == "__main__":
