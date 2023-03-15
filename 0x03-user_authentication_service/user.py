ee799055fd8ab0846f20c0908938e27aa9a3b0d3
ngugimuchangi <tech.curious14@gmail.com>
2023-03-15 08:39:01 +0300

    removed unnucessary dunder init

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -18,15 +18,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
-
-    def __init__(self, email: str, hashed_password: str,
-                 session_id: str = None, reset_token: str = None,
-                 ) -> None:
-        """ Create new User instance
-        """
-        self.email = email
-        self.hashed_password = hashed_password
-        if session_id:
-            self.session_id = session_id
-        if reset_token:
-            self.reset_token = reset_token
